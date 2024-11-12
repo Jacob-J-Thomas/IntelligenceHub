56f81561e9b5d11c0f5cff77da2bb22c94db9d78
Jacob-J-Thomas <92639671+Jacob-J-Thomas@users.noreply.github.com>
2024-11-11 22:28:40 -0600

    Created a competency module to test model outputs (#8)
    Co-authored-by: 98jak <98jak@DESKTOP-RQPA3QI>

---
+++ LoadTesting/Config/StressTestingSettings.cs
@@ -0,0 +1,12 @@
+﻿
+using IntelligenceHub.Tests.Common.Config;
+
+namespace IntelligenceHub.Tests.Stress.Config
+{
+    public class StressTestingSettings
+    {
+        public int TotalRequests { get; set; }
+        public int ConcurrencyLevel { get; set; }
+        public int ConcurrencyDelaySeconds { get; set; }
+    }
+}
