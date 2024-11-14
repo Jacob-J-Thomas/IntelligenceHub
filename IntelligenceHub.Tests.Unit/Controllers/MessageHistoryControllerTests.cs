d7dce8b8c69ce297d4ecc80523c2a0586473516c
98jak <98jak@DESKTOP-RQPA3QI>
2024-11-13 18:17:00 -0600

    Updated folder structure to match project structure

---
+++ IntelligenceHub.Tests.Unit/Controllers/MessageHistoryControllerTests.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using IntelligenceHub.API.DTOs;
+using IntelligenceHub.Controllers;
+using IntelligenceHub.Business;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Xunit;
+using IntelligenceHub.Common.Config;
+
+namespace IntelligenceHub.Tests.Unit.Controllers
+{
+    public class MessageHistoryControllerTests
+    {
+        private readonly Mock<IMessageHistoryLogic> _mockMessageHistoryLogic;
+        private readonly MessageHistoryController _controller;
+
+        public MessageHistoryControllerTests()
+        {
+            // Mock the MessageHistoryLogic
+            _mockMessageHistoryLogic = new Mock<IMessageHistoryLogic>();
+            _controller = new MessageHistoryController(_mockMessageHistoryLogic.Object);
+        }
+
+        #region GetConversation Tests
+        [Fact]
+        public async Task GetConversation_ReturnsBadRequest_WhenCountIsLessThanOne()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            var count = 0;
+
+            // Act
+            var result = await _controller.GetConversation(id, count);
+
+            // Assert
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            Assert.Equal("count must be greater than 1", badRequestResult.Value);
+        }
+
+        [Fact]
+        public async Task GetConversation_ReturnsNotFound_WhenConversationDoesNotExist()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            var count = 1;
+            _mockMessageHistoryLogic.Setup(x => x.GetConversationHistory(id, count)).ReturnsAsync((List<Message>)null);
+
+            // Act
+            var result = await _controller.GetConversation(id, count);
+
+            // Assert
+            var notFoundResult = Assert.IsType<NotFoundObjectResult>(result);
+            Assert.Equal($"The conversation '{id}' does not exist or is empty...", notFoundResult.Value);
+        }
+
+        [Fact]
+        public async Task GetConversation_ReturnsOk_WhenConversationExists()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            var count = 1;
+            var messages = new List<Message> { new Message { Role = IntelligenceHub.Common.GlobalVariables.Role.User, Content = "Test" } };
+            _mockMessageHistoryLogic.Setup(x => x.GetConversationHistory(id, count)).ReturnsAsync(messages);
+
+            // Act
+            var result = await _controller.GetConversation(id, count);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            Assert.Equal(messages, okResult.Value);
+        }
+        #endregion
+
+        #region UpsertConversationData Tests
+        [Fact]
+        public async Task UpsertConversationData_ReturnsBadRequest_WhenMessagesAreNull()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            List<Message> messages = null;
+
+            // Act
+            var result = await _controller.UpsertConversationData(id, messages);
+
+            // Assert
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            Assert.Equal("Messages must be included in the request.", badRequestResult.Value); // assuming this is the case in your controller.
+        }
+
+        [Fact]
+        public async Task UpsertConversationData_ReturnsOk_WhenMessagesAreSuccessfullyAdded()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            var messages = new List<Message> { new Message { Role = IntelligenceHub.Common.GlobalVariables.Role.User, Content = "Test" } };
+            _mockMessageHistoryLogic.Setup(x => x.UpdateOrCreateConversation(id, messages)).ReturnsAsync(messages);
+
+            // Act
+            var result = await _controller.UpsertConversationData(id, messages);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            Assert.Equal(messages, okResult.Value);
+        }
+        #endregion
+
+        #region DeleteConversation Tests
+        [Fact]
+        public async Task DeleteConversation_ReturnsNotFound_WhenConversationDoesNotExist()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            _mockMessageHistoryLogic.Setup(x => x.DeleteConversation(id)).ReturnsAsync(false);
+
+            // Act
+            var result = await _controller.DeleteConversation(id);
+
+            // Assert
+            var notFoundResult = Assert.IsType<NotFoundObjectResult>(result);
+            Assert.Equal($"No conversation with ID '{id}' was found", notFoundResult.Value);
+        }
+
+        [Fact]
+        public async Task DeleteConversation_ReturnsOk_WhenConversationIsDeleted()
+        {
+            // Arrange
+            var id = Guid.NewGuid();
+            _mockMessageHistoryLogic.Setup(x => x.DeleteConversation(id)).ReturnsAsync(true);
+
+            // Act
+            var result = await _controller.DeleteConversation(id);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            Assert.True((bool)okResult.Value);
+        }
+        #endregion
+
+        #region DeleteMessage Tests
+        [Fact]
+        public async Task DeleteMessage_ReturnsNotFound_WhenMessageDoesNotExist()
+        {
+            // Arrange
+            var conversationId = Guid.NewGuid();
+            var messageId = 1;
+            _mockMessageHistoryLogic.Setup(x => x.DeleteMessage(conversationId, messageId)).ReturnsAsync(false);
+
+            // Act
+            var result = await _controller.DeleteMessage(conversationId, messageId);
+
+            // Assert
+            var notFoundResult = Assert.IsType<NotFoundObjectResult>(result);
+            Assert.Equal("The conversation or message was not found", notFoundResult.Value);
+        }
+
+        [Fact]
+        public async Task DeleteMessage_ReturnsOk_WhenMessageIsDeleted()
+        {
+            // Arrange
+            var conversationId = Guid.NewGuid();
+            var messageId = 1;
+            _mockMessageHistoryLogic.Setup(x => x.DeleteMessage(conversationId, messageId)).ReturnsAsync(true);
+
+            // Act
+            var result = await _controller.DeleteMessage(conversationId, messageId);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            Assert.True((bool)okResult.Value);
+        }
+        #endregion
+    }
+}
---
+++ IntelligenceHub.Tests.Unit/Controllers/RagControllerTests.cs
@@ -0,0 +1,228 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using IntelligenceHub.Controllers;
+using IntelligenceHub.Business;
+using IntelligenceHub.API.DTOs.RAG;
+
+namespace IntelligenceHub.Tests.Unit.Controllers
+{
+    public class RagControllerTests
+    {
+        private readonly Mock<IRagLogic> _mockRagLogic;
+        private readonly RagController _controller;
+
+        public RagControllerTests()
+        {
+            _mockRagLogic = new Mock<IRagLogic>();
+            _controller = new RagController(_mockRagLogic.Object);
+        }
+
+        [Fact]
+        public async Task Get_ValidName_ReturnsOkResult()
+        {
+            // Arrange
+            var name = "testIndex";
+            var expectedResponse = new IndexMetadata();
+            _mockRagLogic.Setup(r => r.GetRagIndex(name)).ReturnsAsync(expectedResponse);
+
+            // Act
+            var result = await _controller.Get(name);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            Assert.Equal(expectedResponse, okResult.Value);
+        }
+
+        [Fact]
+        public async Task Get_ValidName_ReturnsNotFound()
+        {
+            // Arrange
+            var name = "testIndex";
+            _mockRagLogic.Setup(r => r.GetRagIndex(name)).ReturnsAsync((IndexMetadata)null);
+
+            // Act
+            var result = await _controller.Get(name);
+
+            // Assert
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Fact]
+        public async Task GetAll_ValidRequest_ReturnsOkResult()
+        {
+            // Arrange
+            var expectedResponse = new List<IndexMetadata> { new IndexMetadata() };
+            _mockRagLogic.Setup(r => r.GetAllIndexesAsync()).ReturnsAsync(expectedResponse);
+
+            // Act
+            var result = await _controller.GetAll();
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            Assert.Equal(expectedResponse, okResult.Value);
+        }
+
+        [Fact]
+        public async Task GetAll_NoIndexes_ReturnsNotFound()
+        {
+            // Arrange
+            _mockRagLogic.Setup(r => r.GetAllIndexesAsync()).ReturnsAsync(new List<IndexMetadata>());
+
+            // Act
+            var result = await _controller.GetAll();
+
+            // Assert
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Fact]
+        public async Task CreateIndex_ValidRequest_ReturnsOkResult()
+        {
+            // Arrange
+            var indexDefinition = new IndexMetadata();
+            _mockRagLogic.Setup(r => r.CreateIndex(indexDefinition)).ReturnsAsync(true);
+
+            // Act
+            var result = await _controller.CreateIndex(indexDefinition);
+
+            // Assert
+            Assert.IsType<OkResult>(result);
+        }
+
+        [Fact]
+        public async Task CreateIndex_FailedRequest_ReturnsBadRequest()
+        {
+            // Arrange
+            var indexDefinition = new IndexMetadata();
+            _mockRagLogic.Setup(r => r.CreateIndex(indexDefinition)).ReturnsAsync(false);
+
+            // Act
+            var result = await _controller.CreateIndex(indexDefinition);
+
+            // Assert
+            Assert.IsType<BadRequestResult>(result);
+        }
+
+        [Fact]
+        public async Task DeleteIndex_ValidRequest_ReturnsOkResult()
+        {
+            // Arrange
+            var index = "testIndex";
+            _mockRagLogic.Setup(r => r.DeleteIndex(index)).ReturnsAsync(true);
+
+            // Act
+            var result = await _controller.DeleteIndex(index);
+
+            // Assert
+            Assert.IsType<OkResult>(result);
+        }
+
+        [Fact]
+        public async Task DeleteIndex_FailedRequest_ReturnsNotFound()
+        {
+            // Arrange
+            var index = "testIndex";
+            _mockRagLogic.Setup(r => r.DeleteIndex(index)).ReturnsAsync(false);
+
+            // Act
+            var result = await _controller.DeleteIndex(index);
+
+            // Assert
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Fact]
+        public async Task GetDocument_ValidRequest_ReturnsOkResult()
+        {
+            // Arrange
+            var index = "testIndex";
+            var document = "doc1";
+            var expectedResponse = new IndexDocument();
+            _mockRagLogic.Setup(r => r.GetDocument(index, document)).ReturnsAsync(expectedResponse);
+
+            // Act
+            var result = await _controller.GetDocument(index, document);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            Assert.Equal(expectedResponse, okResult.Value);
+        }
+
+        [Fact]
+        public async Task GetDocument_ValidRequest_ReturnsNotFound()
+        {
+            // Arrange
+            var index = "testIndex";
+            var document = "doc1";
+            _mockRagLogic.Setup(r => r.GetDocument(index, document)).ReturnsAsync((IndexDocument)null);
+
+            // Act
+            var result = await _controller.GetDocument(index, document);
+
+            // Assert
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        [Fact]
+        public async Task UpsertDocuments_ValidRequest_ReturnsOkResult()
+        {
+            // Arrange
+            var index = "testIndex";
+            var documentUpsertRequest = new RagUpsertRequest();
+            _mockRagLogic.Setup(r => r.UpsertDocuments(index, documentUpsertRequest)).ReturnsAsync(true);
+
+            // Act
+            var result = await _controller.UpsertDocuments(index, documentUpsertRequest);
+
+            // Assert
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task UpsertDocuments_FailedRequest_ReturnsBadRequest()
+        {
+            // Arrange
+            var index = "testIndex";
+            var documentUpsertRequest = new RagUpsertRequest();
+            _mockRagLogic.Setup(r => r.UpsertDocuments(index, documentUpsertRequest)).ReturnsAsync(false);
+
+            // Act
+            var result = await _controller.UpsertDocuments(index, documentUpsertRequest);
+
+            // Assert
+            Assert.IsType<BadRequestResult>(result);
+        }
+
+        [Fact]
+        public async Task DeleteDocuments_ValidRequest_ReturnsOkResult()
+        {
+            // Arrange
+            var index = "testIndex";
+            var commaDelimitedDocNames = "doc1,doc2";
+            var expectedResponse = 2;
+            _mockRagLogic.Setup(r => r.DeleteDocuments(index, It.IsAny<string[]>())).ReturnsAsync(expectedResponse);
+
+            // Act
+            var result = await _controller.DeleteDocuments(index, commaDelimitedDocNames);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            Assert.Equal(expectedResponse, okResult.Value);
+        }
+
+        [Fact]
+        public async Task DeleteDocuments_FailedRequest_ReturnsNotFound()
+        {
+            // Arrange
+            var index = "testIndex";
+            var commaDelimitedDocNames = "doc1,doc2";
+            _mockRagLogic.Setup(r => r.DeleteDocuments(index, It.IsAny<string[]>())).ReturnsAsync(0);
+
+            // Act
+            var result = await _controller.DeleteDocuments(index, commaDelimitedDocNames);
+
+            // Assert
+            Assert.IsType<NotFoundResult>(result);
+        }
+    }
+}
