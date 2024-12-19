764b1cc72bd5922a60e07983ff5fba9f7b219960
Jacob-J-Thomas <92639671+Jacob-J-Thomas@users.noreply.github.com>
2024-12-18 23:05:10 -0600

    Added additional projects to the solution to ensure complete seperation of dependency layers, and updated one circular reference between the Common and API layers

---
+++ IntelligenceHub.Business/Handlers/IValidationHandler.cs
@@ -0,0 +1,15 @@
+﻿using IntelligenceHub.API.DTOs;
+using IntelligenceHub.API.DTOs.Tools;
+
+namespace IntelligenceHub.Business.Handlers
+{
+    public interface IValidationHandler
+    {
+
+        public string? ValidateChatRequest(CompletionRequest chatRequest);
+        public string? ValidateAPIProfile(Profile profile);
+        public string? ValidateBaseDTO(Profile profile);
+        public string? ValidateTool(Tool tool);
+        public string? ValidateProperties(Dictionary<string, Property> properties);
+    }
+}
